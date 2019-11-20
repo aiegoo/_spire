f44eff8ec159c91a745eca98f037f138cff7e70a
DeeDee-CX330 <57964104+DeeDee-CX330@users.noreply.github.com>
2019-11-20 02:57:11 +0100

    Add files via upload

---
+++ Spire/server_update/server_update.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace server_update
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var script_name = "";
+
+            if(args.Length != 1){
+                Console.WriteLine("Script name needed as an argument");
+                Environment.Exit(1);
+            } else {
+                script_name = args[0];
+            }
+
+            using var client = new HttpClient();
+
+            var hosts_file = "slavelist.txt";
+            var hosts = File.ReadLines(hosts_file);
+
+            foreach(var line in hosts){
+                try{
+                    Console.WriteLine("\nRunning script {0} on host {1}", script_name, line);
+                    var data = new Dictionary<string, string>{{"fname", script_name}};
+                    var tosend = new FormUrlEncodedContent(data);
+                    var send = await client.PostAsync(line, tosend);
+                    var response = await send.Content.ReadAsStringAsync();
+                    Console.WriteLine("{0}\n", response);
+                } catch (Exception e) {
+                    Console.WriteLine("An error has occured: {0}", e);
+                }
+            }
+        }
+    }
+}
---
+++ Spire/slave/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace slave.Controllers
+{
+	public class HomeController : Controller
+	{
+
+		[Route("/")]
+		public ActionResult Index (){
+			return NotFound();
+		}
+
+		[Route("/")]
+		[STAThread]
+		[HttpPost]
+		public ActionResult PostIndex()
+		{
+			try{
+				var filename = HttpContext.Request.Form["fname"];
+				var path = String.Format("scripts/{0}", filename);
+				Process.Start(path);
+				return Content("Script successfully executed!");
+			} catch (Exception e){
+				var err = String.Format("An error has occured: {0}", e);
+				return Content(err);
+			}
+		}
+	}
+}
